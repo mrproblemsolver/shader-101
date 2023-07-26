d81d614923622042a9704143a67db67e86fbbd3e
mrproblemsolver <102019204+mrproblemsolver@users.noreply.github.com>
2023-07-26 16:58:46 +0300

    Add files via upload

---
+++ java/ThreeTonedMask.java
@@ -0,0 +1,56 @@
+package shader101;
+public class ThreeTonedMask {
+    public static UnpackedColor filterColor(
+        UnpackedColor refColor,
+        UnpackedColor subColor,
+        UnpackedColor mediumColor,
+        UnpackedColor supColor
+        ) {
+        float[] distanceA;
+        float[] distanceR;
+        float[] distanceG;
+        float[] distanceB;
+        UnpackedColor ret = new UnpackedColor();
+        ret.A = filterChannel(
+            refColor.A,
+            subColor.A,
+            mediumColor.A,
+            supColor.A
+            );
+        ret.R = filterChannel(
+            refColor.R,
+            subColor.R,
+            mediumColor.R,
+            supColor.R
+            );
+        ret.G = filterChannel(
+            refColor.G,
+            subColor.G,
+            mediumColor.G,
+            supColor.G
+            );
+        ret.B = filterChannel(
+            refColor.B,
+            subColor.B,
+            mediumColor.B,
+            supColor.B
+            );
+        return ret;
+        }
+    public static float filterChannel(
+        float refChannel,
+        float subChannel,
+        float mediumChannel,
+        float supChannel
+        ) {
+        if (refChannel == 0.0f) return subChannel;
+        if (refChannel == 0.5f) return mediumChannel;
+        if (refChannel == 1.0f) return supChannel;
+        if (refChannel < 0.5) {
+            return ((mediumChannel - subChannel) * (refChannel * 2)) + subChannel;
+            }
+        else {
+            return ((supChannel - mediumChannel) * ((refChannel - 0.5) * 2)) + mediumChannel;
+            }
+        }
+    }
